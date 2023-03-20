48824892cdc8d53a7053b48b4c9e5fa93d614dae
Tej-Deep <ptejdeep@gmail.com>
2023-03-21 00:50:37 +0800

    Created and tested the training code for CNNs

--- preprocess/datasets.py
+++ preprocess/datasets.py
@@ -5,11 +5,11 @@ class PTBDataset(Dataset):
     def __init__(self, X, df, Y):
         self.labels = Y
         self.data = X
-        self.id = df.index.to_list()
+        # self.id = df.index.to_list()
         self.notes = df.report.to_list()
 
     def __getitem__(self, i):
-        return self.data[i] , self.labels[i], self.notes[self.id[i]]
+        return self.data[i] , self.labels[i], self.notes[i]
 
     def __len__(self):
         return len(self.labels)
